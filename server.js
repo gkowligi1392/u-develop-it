76f865ab7c84ef758cef914bb1149947715b7a25
Gokul's Laptop <gkowligi@gmail.com>
2021-10-20 22:51:14 -0700

    returned data in candidates table

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // import express
 const express = require('express');
+// import mysql2
+const mysql = require('mysql2');
 
 // add port designation and app expression
 const PORT = process.env.PORT || 3001;
@@ -8,6 +10,24 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to MySQL Database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username
+        user: 'root',
+        // MySQL password
+        password: 'Slay1392!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// return data in candidates table
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 // Default response for any other request (Not Found)
 // Make sure this is the last route!!
